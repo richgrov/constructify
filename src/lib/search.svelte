64fe520474c56aabd6bda0e25c8e63211dd0020f
Richard Grover <38992201+richgrov@users.noreply.github.com>
2023-05-03 16:00:57 -0600

    Loading animation for search bar

--- src/lib/search.svelte
+++ src/lib/search.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import IconHammer from '~icons/fa6-solid/hammer';
+	import IconSpinner from '~icons/fa6-solid/spinner';
 
 	let textElement: HTMLInputElement;
 	export let onSubmit: (query: string) => void;
+	export let loading: boolean;
 
 	function submit() {
 		if (!textElement.value) {
@@ -16,10 +18,15 @@
 <form on:submit={submit} class="flex relative rounded-full border-black border-2
 	shadow-[5px_5px_black] bg-white">
 	<input type="text" placeholder="A house made of gold" bind:this={textElement}
-		class="w-full p-5 rounded-full outline-none">
+		class="w-full p-5 rounded-full outline-none {loading ? 'opacity-50' : ''}"
+		disabled={loading}>
 	<button class="flex items-center mr-3">
 		<div class="bg-pink-300 rounded-full p-3">
-			<IconHammer />
+			{#if loading}
+				<IconSpinner class="animate-spin" />
+			{:else}
+				<IconHammer />
+			{/if}
 		</div>
 	</button>
 </form>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,21 +10,26 @@
 		connectFunctionsEmulator(functions, 'localhost', 5001);
 	}
 
+	let loading = false;
 	async function submit(prompt: string) {
+		loading = true;
+
 		try {
 			const result = await generate({ prompt });
 			const jobId = result.data.jobId;
 			window.location.href = `${window.location.pathname}/view?id=${jobId}`;
 		} catch (err) {
 			console.error(err);
+		} finally {
+			loading = false;
 		}
 	}
 </script>
 
 <div class="flex justify-center items-center flex-col h-4/5">
 	<h1 class="text-white text-5xl pb-10">constructify!</h1>
-	<div class="w-4/5 max-w-2xl">
-		<Search onSubmit={submit} />
+	<div class="w-2/5">
+		<Search onSubmit={submit} loading={loading} />
 	</div>
 </div>
 
